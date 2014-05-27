2f297926bfb051914733563f7831bbb762d9cfde
Xero <volleyp7689@gmail.com>
2014-05-27 10:45:46 +0800

    remove some file don't need track

--- staticRoot/js/work.js
+++
@@ -1,2 +0,0 @@
-// http://masonry.desandro.com/masonry.pkgd.js added as external resource
-// https://rawgithub.com/desandro/classie/master/classie.js added
--- staticRoot/myEffect.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$(".indexPost").hover(function(){
-		$(this).animate({zoom: '125%'}, 'fast');
-		},
-		function(){
-		$(this).animate({zoom: '100%'}, 'fast');
-	});
-});
