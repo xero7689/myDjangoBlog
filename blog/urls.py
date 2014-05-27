891e353c76d8bb856ff1625ed58ea3a769fb67d5
Xero <volleyp7689@gmail.com>
2014-05-27 10:42:23 +0800

    remove unused url

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import patterns, include, url
 from blog import views
 
 urlpatterns = patterns('',
-    url(r'^$', 'blog.views.index'),
-    url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post'),
+
 )
